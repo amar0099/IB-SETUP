4925b0d4378e05826e197b52622d8606f8e89045
amar0099 <43070325+amar0099@users.noreply.github.com>
2026-04-20 20:46:31 +0530

    Update fyers_feed.py

--- core/fyers_feed.py
+++ core/fyers_feed.py
@@ -236,7 +236,7 @@ class FyersFeed:
         while not self._stop_flag.is_set():
             try:
                 self._ws = data_ws.FyersDataSocket(
-                    access_token=f"{self.app_id}:{self.access_token}"  # ✅ CORRECT
+                    access_token=f"{self.app_id}:{self.access_token}",
                     log_path="",
                     litemode=False,
                     write_to_file=False,
